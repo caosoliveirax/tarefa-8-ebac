c1258bd1145edd44b7f7adaeacb24150939a5fe0
Carlos <kaduchaos18@gmail.com>
2024-09-24 18:27:41 -0300

    adição: agora os contatos aparecem na lista conforme a tarefa e alguns retoques para organizar o codigo

--- main.js
+++ main.js
@@ -2,29 +2,55 @@ const form = document.getElementById('form-agenda');
 const nomeCompleto = document.getElementById('nome-contato');
 const telefoneFormatado = document.getElementById('numero-contato');
 
-
 let FormEValido = false;
 
+let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    validaNome();
+    if (validaNome()) {
+        adicionaLinha();
+        atualizaTabela();
+        limpaCampos();
+    }
 });
 
-//Formatação de numero de telefone conforme digita no campo Telefone
 telefoneFormatado.addEventListener('input', function (e) {
     const x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
     e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
 });
 
-//Função que verifica se o nome está com mais de uma parte, o que configura que seja um nome completo
 function validaNome() {
     if (nomeCompleto.value.trim().split(' ').length > 1) {
-        nomeCompleto.value = ''; // Limpa o campo de nome
-        telefoneFormatado.value = ''; // Limpa o campo de telefone
+        nomeCompleto.style.border = '';
+        document.querySelector('.error-message').style.display = 'none';
+        return true;
     } else {
         nomeCompleto.style.border = '1px solid red'
         document.querySelector('.error-message').style.display = 'block';
+        return false;
     }
+}
+
+function adicionaLinha() {
+    const inputNomeCompleto = document.getElementById('nome-contato');
+    const inputTelefone = document.getElementById('numero-contato');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeCompleto.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function limpaCampos() {
+    nomeCompleto.value = ''; 
+    telefoneFormatado.value = ''; 
 }
