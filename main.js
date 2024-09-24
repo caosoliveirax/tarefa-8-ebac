50dbbf7f93cf69c061ace772a6f12fc747602483
Carlos <kaduchaos18@gmail.com>
2024-09-24 16:27:05 -0300

    adição: agora há a formatação automatica como um numero de telefone conforme digitação no campo

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('form-agenda');
 const nomeCompleto = document.getElementById('nome-contato');
+const telefoneFormatado = document.getElementById('numero-contato');
+
 
 let FormEValido = false;
 
@@ -9,6 +11,11 @@ form.addEventListener('submit', function(e) {
 
     validaNome();
 
+});
+
+telefoneFormatado.addEventListener('input', function (e) {
+    const x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
 });
 
 function validaNome() {
